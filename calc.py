5f13a99407c1e0da0a19c922d67bf23f72b82101
DeVoncs <114286948+DeVoncs@users.noreply.github.com>
2023-03-03 21:56:25 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,17 @@
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("You select adding: ")
+    print(num1 + num2)
+elif op == "-":
+    print("You select subtraction: ")
+    print(num1 - num2)
+elif op == "*":
+    print("You select multiplication: ")
+    print(num1 * num2)
+elif op == "/":
+    print("You select division: ")
+    print(num1 / num2)
